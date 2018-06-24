e1804c2b721ca48116896ea5e3af69b43594e82b
Joshua Magee <magee.joshua@yahoo.com>
2018-06-24 13:15:05 -0700

    Import of project skeleton.

---
+++ app/water-cooler.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import WaterCooler
+
+main :: IO ()
+main = someFunc
---
+++ lib/WaterCooler.hs
@@ -0,0 +1,6 @@
+module WaterCooler
+( someFunc
+) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
